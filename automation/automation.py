ce5c6aadfc43543329383d4fa238703cca3613e2
Khaled <k.bachour@nottingham.ac.uk>
2012-08-23 11:12:53 +0100

    Added code for automated call handling

--- automation/automation.py
+++ automation/automation.py
@@ -1,6 +1,8 @@
 import random
 from sets import Set
 from cargoapp.models import Extra, User
+import time
+from threading import Thread
 
 def determineSwipeSideConditions(user, reader): 
     atRecommendedStation = False
@@ -73,24 +75,74 @@ def processRules(rules, user, param):
 		if rule == 'RULE_5':
 			callee = pickRecipient(user.group, user)
 			# Make a call with delay
+			max_threshold = None
+			max_threshold_value = 0
+			for threshold in Extra.objects.filter(name='PLAYER_THRESHOLD')
+				if user.credit > threshold.value and threshold.value > max_threshold_value
+					max_threshold = threshold
+					max_threshold_value = threshold.value
+				
+			msg = Message.objects.get(name=max_threshold.for_object_id)
+			
+			params = {"points":max_threshold.value, "name":user.name}
+			
+			# Launch new thread to make a call after 60 seconds.
+			t = Thread(target=makeCallWithDelay, args=(callee, msg, params, 45))
+    		t.start()
+			
 		elif rule == 'RULE_10':
 			groups = getGroups()
 			for group in groups:
 				callee = pickRecipient(group, None)
+				if group == user.group
 				# Make a call with delay
+					msg = Message.objects.get('RULE_10A')
+				else
+					msg = Message.objects.get('Rule_10B')
+				params = {}
+				t = Thread(target=makeCallWithDelay, args=(callee, msg, params, 90))
+    			t.start()
+				
 		elif rule == 'RULE_2':
-			probability = Extra.objects.get(name='CALL_PROBABILITY')
-			if flip(probability):
+			try:
+				probability = int(Extra.objects.get(name='CALL_PROBABILITY'))
+			except Exception as e:
+				print e
+			if flip(probability/100.0):
 				# Make a call
+				callee = user
+				location = pickHighValueLocation(user)
+				if location == None #If no location found
+					continue
+				user.goto_location = location.name
+				user.save()
+				params = {'name':user.name,'location':location.name}
+				msg = Message.objects.get(name='RULE_2')
+				makeCall(callee, msg, params)
 			elif:
 				# Don't make a call
+				pass
 		else:
 			if rule == 'RULE_7':
 				# Make a call
+				callee = pickRecipient(user.group, None)
+				msg = Message.objects.get(name='RULE_7')
+				makeCall(callee, msg, {})
+				callee = user
+				msg = Message.objects.get(name='RULE_8')
+				makeCall(callee, msg, {})
 			elif rule == 'RULE_6':
-				# Make a call
+				callee = getCargoInGroup(user.group)
+				msg = Message.objects.get(name='RULE_6')
+				params = {'name':user.name}
+				makeCall(callee, msg, params)
+				sendMessageToCop(user)
 			elif rule == 'RULE_3':
-				# Make a call
+				callee = user
+				params = {'location':user.goto_location, 'name' : user.name}
+				msg = Message.objects.get(name='RULE_3')
+				makeCall(callee, msg, params)
+				
 			
 def getGroups():
 	users = User.objects.all()
@@ -100,20 +152,39 @@ def getGroups():
 	return groups
 	
 def pickRecipient(group_id, not_user):
-	users = User.objects.get(group=group_id)
-	while True:
-		user = users.get(int(random()*len(users)))
+	users = User.objects.filter(group=group_id).order_by('?')
+	for user in user:
 		if not user.isCargo and not user == not_user:
 			return user			
 		
 def makeCall(user, message, params):
+	token = '13acde1200cc5142acde42576458b5b7a48c638058a26304bdf34c476b11647b18c0da3b5ce13ceb7cb852a5';    	
+	msg = Template(message.content).safe_substitute(params)
 	
+	call = Call(callee=user.name, message=msg.name);
+	call.save();
 	
-	
-	
-	
-	
-	
+	try:
+        url = 'http://api.tropo.com/1.0/sessions?action=create&token='+ token + '&numberToCall=' + user.phone_num + '&messageToSay=' + msg.content + '&call_id' + str(call.id)
+        page = urllib.urlopen(url)
+        response = page.read()    
+        print response
+    except Exception as e:
+    	print e;
+    	call.status = -3;
+    	call.save();
+    	
+def makeCallWithDelay(user,message,params,delay):
+	time.sleep(delay)
+	makecCall(user,message,params)
+		
+def getCargoInGroup(groupNo)
+	users = User.objects.filter(group=groupNo)
+	return users.get(isCargo=True)
+
+def sendMessageToCop(user)
+	number = user.phone_num
+	name = user.name
 	
 	
 	
