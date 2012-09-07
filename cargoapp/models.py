2f32e8f400f02e1497ce8ef32440be4df0848a8f
joelfischer <jef@cs.nott.ac.uk>
2012-09-07 14:02:55 +0100

    25 groups

--- cargoapp/models.py
+++ cargoapp/models.py
@@ -4,7 +4,9 @@ class User(models.Model):
     game_name = models.CharField(max_length=30) #Game.name
     name = models.CharField(max_length=50)
     phone_num = models.CharField(max_length=30)
-    group = models.CharField(max_length=30, choices=[('1', '1'), ('2', '2') , ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
+    group = models.CharField(max_length=30, choices=[('1', '1'), ('2', '2') , ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
+                                                     ('11', '11'), ('12', '12'),('13', '13'),('14', '14'),('15', '15'),('16', '16'),('17', '17'),('18', '18'),('19', '19'),('20', '20'),
+                                                     ('21', '21'),('22', '22'),('23', '23'),('24', '24'),('25', '25')])
     alias = models.CharField(max_length=30)
     rfid = models.CharField(max_length=200, editable=False)
     is_cargo = models.BooleanField() 
@@ -18,7 +20,9 @@ class All_User(models.Model):
     game_name = models.CharField(max_length=30) #Game.name
     name = models.CharField(max_length=50)
     phone_num = models.CharField(max_length=30)
-    group = models.CharField(max_length=30, choices=[('1', '1'), ('2', '2') , ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
+    group = models.CharField(max_length=30, choices=[('1', '1'), ('2', '2') , ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
+                                                     ('11', '11'), ('12', '12'),('13', '13'),('14', '14'),('15', '15'),('16', '16'),('17', '17'),('18', '18'),('19', '19'),('20', '20'),
+                                                     ('21', '21'),('22', '22'),('23', '23'),('24', '24'),('25', '25')])
     alias = models.CharField(max_length=30)
     rfid = models.CharField(max_length=200, editable=False)
     is_cargo = models.BooleanField() 
